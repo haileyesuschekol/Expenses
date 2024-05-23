d35ee04356ef247ce176d6507d93f166a209d395
Haileyesus Chekol <teamdevs7@gmail.com>
2024-05-23 14:52:30 +0100

    add form handler

--- react-app/src/components/FormInput.tsx
+++ react-app/src/components/FormInput.tsx
@@ -1,12 +1,29 @@
 import "bootstrap/dist/css/bootstrap.css"
+import { useForm, SubmitHandler } from "react-hook-form"
+
+interface FormData {
+  item: string
+  price: number
+  catagory: string
+}
 
 const FormInput = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>()
+
+  const submitter: SubmitHandler<FormData> = (data) => {
+    console.log(data)
+  }
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit(submitter)}>
         <div className="form-group mb-2">
           <label htmlFor="exampleInputEmail1 mb-2">Item</label>
           <input
+            {...register("item")}
             type="name"
             className="form-control"
             id="item"
@@ -17,6 +34,7 @@ const FormInput = () => {
         <div className="form-group mb-2">
           <label htmlFor="price mb-2">Price</label>
           <input
+            {...register("price")}
             type="number"
             className="form-control"
             id="price"
@@ -26,7 +44,11 @@ const FormInput = () => {
 
         <div className="form-group mb-2">
           <label htmlFor="catagories mb-2">Catagories</label>
-          <select className="form-control" id="catagories">
+          <select
+            className="form-control"
+            id="catagories"
+            {...register("catagory")}
+          >
             <option>Grosery</option>
             <option>Bill</option>
           </select>
